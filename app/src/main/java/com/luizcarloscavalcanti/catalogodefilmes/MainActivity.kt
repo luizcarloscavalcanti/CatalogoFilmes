552c80b851e43fb3ac5435761341214906a9dde0
LUIZ CAVALCANTI <luizcarlos.cavalcanti@outlook.com>
2021-03-03 21:38:08 -0300

    Reestruturação do Zero

---
+++ app/src/main/java/com/luizcarloscavalcanti/catalogodefilmes/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.luizcarloscavalcanti.catalogodefilmes
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+}
--- app/src/main/java/com/luizcarloscavalcanti/catalogodefilmes/data/objects/Movie.kt
+++
@@ -1,5 +0,0 @@
-package com.luizcarloscavalcanti.catalogodefilmes.data.objects
-
-class Movie {
-
-}
